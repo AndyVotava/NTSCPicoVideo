e522c41a54f26760f517001b6875a053ed122ea2
Andy Votava <andycvotava@gmail.com>
2024-07-17 12:31:07 -0500

    "figuring out git"

--- functions.h
+++ functions.h
@@ -4,7 +4,7 @@
 #include <math.h>
 
 
-uint32_t to_pio(int IRE, float time_us){
+uint32_t us_to_pio(int IRE, float time_us){
 
     uint32_t level = roundf((IRE*31)/140);
 
@@ -14,11 +14,35 @@ uint32_t to_pio(int IRE, float time_us){
     
 }
 
+uint32_t ns_to_pio(int IRE, int time_ns){
+
+    uint32_t level = roundf((IRE*31)/140);
+
+    uint32_t time = roundf(((time_ns) - 40)/8);
+
+    return ((time << 5) | level);
+    
+}
+
+
+void data(){
+
+}
+
 void scanline(PIO pio, uint sm){
-    pio_sm_put_blocking(pio, sm, to_pio(40, 1.5));
-    pio_sm_put_blocking(pio, sm, to_pio(0, 4.7));
-    pio_sm_put_blocking(pio, sm, to_pio(40, 4.7));
-    pio_sm_put_blocking(pio, sm, to_pio(140, 4.7));
+    pio_sm_put_blocking(pio, sm, us_to_pio(40, 1.5));
+    pio_sm_put_blocking(pio, sm, us_to_pio(0, 4.7));
+    pio_sm_put_blocking(pio, sm, us_to_pio(40, 4.7));
+    int j = 40;
+
+    for(int i = 0; i < 720; i++){
+        if(j > 140){
+            j = 40;
+        }
+        pio_sm_put_blocking(pio, sm, ns_to_pio(j, 73));
+        j++;
+    }
+    //pio_sm_put_blocking(pio, sm, us_to_pio(140, 52.6));
 
 }
 
--- main.c
+++ main.c
@@ -1,32 +1,7 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "output.pio.h"
-
-//#define fp (92 << 5) | 12
-//#define synctip (292 << 5) | 0
-
-uint32_t bp(){
-    return ((582 << 5) | 9);
-}
-
-uint32_t fp(){
-    return ((182 << 5) | 9);
-}
-
-uint32_t synctip(){
-    return ((582 << 5) | 0);
-}
-
-uint32_t video (){
-    return ((6570 << 5) | 31);
-}
-
-void scanline(PIO pio, uint sm){
-    pio_sm_put_blocking(pio, sm, fp());
-    pio_sm_put_blocking(pio, sm, synctip());
-    pio_sm_put_blocking(pio, sm, bp());
-    pio_sm_put_blocking(pio, sm, video());
-}
+#include "functions.h"
 
 int main(){
 
